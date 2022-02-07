ee5685807b09e3d0d2b668d51c99a717d5fba6b1
Tausif4171 <tausifkhan4171@gmail.com>
2022-02-07 14:54:22 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/home';
+import CreatePost from './pages/createpost';
+import Login from './pages/login';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav, Navbar, Container, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar bg="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#" style={{ fontSize: 26, color: "green" }}>BlogApp</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '200px', wordSpacing: 42 }}
+              navbarScroll
+            >&nbsp;
+
+              <Link to="/" id="fornavbar">Home</Link>&nbsp;
+              <Link to="/createpost" id="fornavbar">CreatePost</Link>&nbsp;
+              <Link to="/login" id="fornavbar">Login</Link>
+
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/createpost' element={<CreatePost />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/firebase-config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+import {getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFJ26PWudezZ0wqINhcQi1q_42wlSfABs",
+  authDomain: "first-blog-website-665ee.firebaseapp.com",
+  projectId: "first-blog-website-665ee",
+  storageBucket: "first-blog-website-665ee.appspot.com",
+  messagingSenderId: "284041964733",
+  appId: "1:284041964733:web:f4e9f39ce8491670721bd4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
