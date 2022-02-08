66f3ac1a0e2247a77378c2ef53c7ba9d1407fe24
Tausif4171 <tausifkhan4171@gmail.com>
2022-02-08 19:11:12 +0530

    CreatePost page form and state created

--- src/pages/createpost.js
+++ src/pages/createpost.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CreatePost = () => {
+    const [title, setTitle] = useState("");
+    const [post, setPost] = useState("");
     return (
+        <div className='container' style={{marginTop:24}}>
+
         <div>
-            <h1>Post</h1>
+            <h1>Create a Post</h1>
+            <div>
+                <label>Title</label><br />
+                <input placeholder='Enter your title...' onChange={(e) =>{
+                   setTitle(e.target.value);
+                }} />
+            </div><br />
+            <div>
+                <label>Post</label><br />
+                <textarea placeholder='Enter your Post...' onChange={(e) =>{
+                   setPost(e.target.value);
+                }}/>
+            </div>
+        </div>
+        
+        <div>
+            <button>Submit Post</button>
+        </div>
+
         </div>
     );
 };
