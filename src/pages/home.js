f75d335f5b2f851f4703195f0ed39a226b935788
Tausif4171 <tausifkhan4171@gmail.com>
2022-02-09 20:46:32 +0530

    Given permissions to users to delete only those post that they have ctreated

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,9 +3,15 @@ import React, { useState, useEffect } from 'react';
 import { auth, db } from '../firebase-config';
 import { Card } from 'react-bootstrap';
 
+
 function Home(props) {
     const [postList, setPostList] = useState([]);
     const postsCollectionRef = collection(db, "posts");
+
+    const postDelete = async (id) => {
+        const postDoc = doc(db, "posts", id);
+        await deleteDoc(postDoc);
+    };
 
     useEffect(() => {
         const getPosts = async () => {
@@ -13,38 +19,31 @@ function Home(props) {
 
             setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))); // we have created a new array(useful data) using map function 
             // because in data array there are lot of data...
-
         }
         getPosts();
-    }, []);
+    }, [postDelete()]);
 
-
-    const postDelete = async (id) => {
-        const postDoc = doc(db, "posts", id);
-        await deleteDoc(postDoc);
-    };
 
     return (
         <div>
-            <h1>{postList.map((post) => {
+            {postList.map((post) => {
                 return (
                     <div className='container'>
-    
+
                         <br />
                         <Card style={{ width: '24rem' }}>
 
                             <Card.Body>
                                 <Card.Title>
                                     <h3>{post.title}</h3>
-
-                                    {   props.isAuth && post.author.id === auth.currentUser.uid && (
+                                </Card.Title>
+                                <div>
+                                    {props.isAuth && post.author.id === auth.currentUser.uid && (
                                         <button onClick={() => {
                                             postDelete(post.id);
-                                        }} style={{ backgroundColor: "white", border: "none" }}>&#128465;</button> )
+                                        }} style={{ backgroundColor: "white", border: "none" }}>&#128465;</button>)
                                     }
-
-
-                                </Card.Title>
+                                </div>
 
                                 <Card.Text>
                                     <h5>{post.post}</h5>
@@ -57,7 +56,7 @@ function Home(props) {
                         </Card>
                     </div>
                 );
-            })}</h1>
+            })}
         </div>
     );
 }
