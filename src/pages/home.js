12a8f1da23b4aa06d7952e088ff5301d44fafb67
Tausif4171 <tausifkhan4171@gmail.com>
2022-02-09 13:19:22 +0530

    state added

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import { collection, doc, getDocs } from 'firebase/firestore';
+import React, { useState, useEffect } from 'react';
+import { db } from '../firebase-config';
 
 function Home(props) {
+    const [postList, setPostList] = useState([]);
+    const getCollectionRef = collection(db, "posts");
+
+    useEffect(() => {
+        const getPosts = async () => {
+            const data = await getDocs(getCollectionRef);
+
+            setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))); // we have created a new array(useful data) using map function 
+                                                                                  // because in data array there are lot of data...
+            
+        }
+        getPosts();
+    }, []);
+
     return (
         <div>
-            <h1>Home</h1>
+
         </div>
     );
 }
